56db27ef424402b46d459d8cc71709fc08d52b0c
Vanessa <maryvanessa200@gmail.com>
2022-11-23 14:46:30 -0300

    Hasta aquí vamos bien

--- app/src/main/java/guerrero/mary/Login.java
+++ app/src/main/java/guerrero/mary/Login.java
@@ -2,16 +2,71 @@ package guerrero.mary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    Button btnRegistro, btnInicio;
+
+    ImageView image;
+    TextView desc, titulo;
+    TextInputLayout correo, contra;
+
+    Vibrator vibrator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_login);
+
+
+        image = findViewById(R.id.chefInicioSesion);
+        titulo = findViewById(R.id.txtAgain);
+        desc = findViewById(R.id.txtHola);
+        correo = findViewById(R.id.username);
+        contra = findViewById(R.id.password);
+
+        btnRegistro = findViewById(R.id.callSignUp);
+        btnInicio = findViewById(R.id.callLogin);
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, UserProfile.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Sign_Up.class);
+                Pair[] pairs = new Pair[6];
+                pairs[0] = new Pair<View, String>(image, "imagenAnimada");
+                pairs[1] = new Pair<View, String>(titulo, "logo_text");
+                pairs[2] = new Pair<View, String>(desc, "logo_desc");
+                pairs[3] = new Pair<View, String>(correo, "username_tran");
+                pairs[4] = new Pair<View, String>(contra, "password_tran");
+                pairs[5] = new Pair<View, String>(btnRegistro, "login_signup_tran");
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this, pairs);
+                    startActivity(intent, options.toBundle());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/guerrero/mary/Sign_Up.java
+++ app/src/main/java/guerrero/mary/Sign_Up.java
@@ -3,12 +3,54 @@ package guerrero.mary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Sign_Up extends AppCompatActivity {
 
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button regBtn, regToLoginBtn;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Acá se capturan las variables para el  activity_sign_up.xml
+        regName = findViewById(R.id.reg_name);
+        regUsername = findViewById(R.id.reg_username);
+        regEmail = findViewById(R.id.reg_email);
+        regPhoneNo = findViewById(R.id.reg_phoneNo);
+        regPassword = findViewById(R.id.reg_password);
+        regBtn = findViewById(R.id.signUpBtn);
+        regToLoginBtn = findViewById(R.id.loginBtn);
+
+
+        //Save data in FireBase on button click
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+
+                //Acá se obtienes las variables
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String phoneNo = regPhoneNo.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+                UserHelperClass helperClass = new UserHelperClass (name, username, email, phoneNo, password);
+                reference.child(username ).setValue(helperClass);
+
+            }
+        });
+
     }
 }
